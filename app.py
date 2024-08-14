5f822e9a54a4876c309e813215116af397577538
kjh0209 <128874645+kjh0209@users.noreply.github.com>
2024-08-14 14:35:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,68 @@
 from flask import Flask, request, jsonify, render_template
 import requests
 from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('./index.html')
 
-# Neis API credentials
 API_KEY = "0e29646040154781950fab3b908ae536"
 BASE_URL = "https://open.neis.go.kr/hub/mealServiceDietInfo"
-SCHOOL_CODE = "8321124"  # Jeonbuk Science High School code
-SCHOOL_REGION = "P10"
-def get_lunch_menu():
+SD_SCHUL_CODE = "8321124"  # 행정표준코드
+ATPT_OFCDC_SC_CODE = "P10" # 시도교육청코드
+
+def get_lunch_menu(): # 오늘 급식
     today = datetime.now().strftime('%Y%m%d')
     params = {
         "KEY": API_KEY,
         "Type": "json",
-        "ATPT_OFCDC_SC_CODE": SCHOOL_REGION,
-        "SD_SCHUL_CODE": SCHOOL_CODE,
+        "ATPT_OFCDC_SC_CODE": ATPT_OFCDC_SC_CODE,
+        "SD_SCHUL_CODE": SD_SCHUL_CODE,
         "MLSV_YMD": today
     }
 
     response = requests.get(BASE_URL, params=params)
     data = response.json()
     try:
-        # Find the lunch menu by checking the MMEAL_SC_NM field for "중식"
         for meal in data["mealServiceDietInfo"][1]["row"]:
-            if meal["MMEAL_SC_NM"] == "중식":  # "중식" means lunch
+            if meal["MMEAL_SC_NM"] == "조식":
+                menu = meal["DDISH_NM"].replace("<br/>", "\n")
+                return menu
+            elif meal["MMEAL_SC_NM"] == "중식":
+                menu = meal["DDISH_NM"].replace("<br/>", "\n")
+                return menu
+            elif meal["MMEAL_SC_NM"] == "석식":
                 menu = meal["DDISH_NM"].replace("<br/>", "\n")
                 return menu
         
-        return "No lunch menu available for today."
+        return "메뉴를 불러오는데 실패했습니다."
     except (KeyError, IndexError):
-        # Return a default message if the expected data isn't found
-        return "No lunch menu available for today."
+        return "메뉴를 불러오는데 실패했습니다."
 
 
-@app.route('/message', methods=['POST'])
+@app.route('/menu', methods=['POST']) # 급식
 def message():
-    # Always fetch the lunch menu
     response_text = get_lunch_menu()
+    response_data = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": response_text
+                    }
+                }
+            ]
+        }
+    }
 
-    # Construct the response to be sent back to KakaoTalk
+    return jsonify(response_data)
+
+@app.route('/hello', methods=['GET']) # 인사
+def message():
+    response_text = "안녕하세요, 현재 datetime은", datetime.now().strftime("%Y%m%d")
     response_data = {
         "version": "2.0",
         "template": {
